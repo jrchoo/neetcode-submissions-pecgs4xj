86a24ffc2e451207df4225438065de4f0a9b4846
Choo Jia Rong <122244985+jrchoo@users.noreply.github.com>
2026-04-02 17:38:07 +0800

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double result = 1.0;
+        long exponent = Math.abs(n);
+        while (exponent > 0) {
+            if (exponent % 2 == 1) {
+                result *= x;
+            }
+
+            x *= x;
+            exponent /= 2;
+        }
+
+        return n > 0 ? result : 1 / result;
+    }
+}
