3dd52172b31ec35dc8d2e14f1a6e2397f7beffee
Choo Jia Rong <122244985+jrchoo@users.noreply.github.com>
2026-07-13 03:05:58 +0800

    Add: matchsticks-to-square - submission-1

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-1.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean makesquare(int[] matchsticks) {
+        int sum = 0;
+        // check if the length of matchsticks can form a square
+        for (int stick : matchsticks) {
+            sum += stick;
+        }
+        // if sum of lengths is not divisible by 4, unable to form a square
+        if (sum == 0 || sum % 4 != 0) {
+            return false;
+        }
+
+        // buckets to hold the sides of the square
+        int[] sides = new int[4];
+        int target = sum / 4;
+        // sort array in descending order to fit the longer sticks first
+        Arrays.sort(matchsticks);
+        
+        return backtrack(matchsticks, 0, sides, target);
+    }
+
+    private boolean backtrack(int[] matchsticks, int index, int[] sides, int target) {
+        // successfully placed all matchsticks if end of the array reached
+        if (index == matchsticks.length) {
+            return true;
+        }
+
+        int current = matchsticks[index];
+        // check whether the current matchstick can be placed at this side
+        for (int i = 0; i < 4; i++) {
+            if (sides[i] + current > target) { // exceeded the limit, move on to the next side
+                continue;
+            }
+            // choose
+            sides[i] += current;
+            // explore
+            if (backtrack(matchsticks, index + 1, sides, target)) {
+                return true;
+            }
+            // unchoose
+            sides[i] -= current;
+        }
+
+        return false;
+    }
+}
